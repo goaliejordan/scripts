3701c0bfd9ee35ef32333a9a4439e8d0307cce36
goaliejordan <goaliejordan@gmail.com>
2017-11-15 15:24:59 -0700

    Add files via upload
    Player mmr for HOTS

---
+++ python/player_function.py
@@ -0,0 +1,31 @@
+## collects MMR(rank) from www.hotslog.com for listed players and print it to the screen.
+import requests
+import datetime
+hots_players = {"goaliejordan": "6040022",
+           "rice": "3987504",
+           "deathstrkr14": "6047137"}
+
+
+def get_mmr(players):
+    today = datetime.date.today()
+    print("Date = " + str(today))
+    for player in players:
+        url = "https://api.hotslogs.com/Public/Players/" + players[player]
+        response = requests.get(url)
+        # print(response.status_code)
+        data = response.json()
+        name = data["Name"]
+        mmrs = data["LeaderboardRankings"]
+        if response.status_code == 200:
+            print("&&" * len(mmrs[0]["GameMode"] + ": " + str(mmrs[0]["CurrentMMR"])))
+            print("Player name: " + name)
+            for mmr in mmrs:
+                print("*-" * (len(mmr["GameMode"] + ": " + str(mmr["CurrentMMR"]))))
+                print(mmr["GameMode"] + ": " + str(mmr["CurrentMMR"]))
+            print("&&" * len(mmr["GameMode"] + ": " + str(mmr["CurrentMMR"])))
+            print("")
+        else:
+            print("Something went wrong with the request")
+
+get_mmr(hots_players)
+
